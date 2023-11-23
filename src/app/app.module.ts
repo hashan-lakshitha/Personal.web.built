d562e1becc70d5693d2e0f5cb1134466cea90b00
hashan-lakshitha <r123tharaka74@gmail.com>
2023-11-23 15:27:16 +0530

    init 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './pageComponent/navbar/navbar.component';
+import { MainHomeComponent } from './pageComponent/main-home/main-home.component';
+import { SocialMediaComponent } from './pageComponent/social-media/social-media.component';
+import { ExampleComponent } from './pageComponent/example/example.component';
+import { AboutComponent } from './pageComponent/about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainHomeComponent,
+    SocialMediaComponent,
+    ExampleComponent,
+    AboutComponent
+
+
+
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
